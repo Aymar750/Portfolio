12443ad62c1fdf982aee332538727ada6a681fdd
Aymar <yannyao4066@gmail.com>
2023-03-05 06:09:16 +0000

    Part3 portfolio fini

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,15 +33,57 @@ tabs.forEach((tab) => {
 
 });
 
-
-
-/* MIXITUP FILTER PORTFOLIO */
-
+/* MIXITUP FILTER PORTFOLIO
+ici assez simple j'ai juste mis le code qui me permet de 
+faire un filtrage de mes apps dans la section 
+portfolio avec une petite animation de 0.3s
+*/
+let mixerPortfolio = mixitup('.work_container',{
+    selectors: {
+        target: '.work_card',
+    },
+    animation:{
+        duration: 300,
+    }
+});
 
 /* Link Active Work */
+const linkWork= document.querySelectorAll('.work_item');
+function linkActiveWork(){
+    linkWork.forEach(l=> l.classList.remove('active-work'));
+    this.classList.add('active-work');
+    
+}
+
+linkWork.forEach((link) => {link.addEventListener('click',linkActiveWork)});
 
 
-/* Work Popup */
+/* Work Popup 
+ici ca me retourne un popup avec le contenu 
+du projet
+
+
+*/
+document.addEventListener('click', (e) => {
+    if (e.target.classList.contains('work_button')) {
+        togglePortfolio();
+        portfolioDetails(e.target.parentElement);
+    }
+});
+
+function togglePortfolio() {
+    document.querySelector('.portfolio_popup').classList.toggle('open');
+}
+document.querySelector('.portfolio_popup-close').addEventListener('click', togglePortfolio);
+
+function portfolioDetails(portfolioItem) {
+    document.querySelector('.pp_thumbnail img').src= portfolioItem.querySelector('.work_img').src;
+    ;
+    document.querySelector('.portfolio_popup-subtitle span').innerHTML= portfolioItem.querySelector('.work_title').innerHTML;
+    document.querySelector('.portfolio_popup-body').innerHTML= portfolioItem.querySelector('.portfolio_item-details').innerHTML;
+
+
+}
 
 
 /* SERVICES MODAL */
